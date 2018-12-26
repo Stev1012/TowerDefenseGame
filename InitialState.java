f77fd10f215d04ed46d967b5f9e15ed52ef8b0c7
Michael Ellis <32455984+FinnyTheFatFish@users.noreply.github.com>
2018-12-26 01:44:42 -0600

    Pull (#5)
    * Delete test
    
    * Create README.md
    
    * Create test3
    
    * Update README.md
    
    * Update README.md
    
    * Delete test2
    
    * Delete test3
    
    * Add files via upload
    
    Adding State interface and beginnings of state controller as well as frames for a few states

---
+++ InitialState.java
@@ -0,0 +1,42 @@
+
+public class InitialState implements State {
+
+	private String name = "initial state";
+	
+	@Override
+	public void enter() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processInput() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+}
